bbdadfd89a309662c24063756cf9e4e9b9a358aa
Rxlling-Pxly <44181024+Rxlling-Pxly@users.noreply.github.com>
2026-01-21 15:05:17 -0800

    Built project for the first time

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Justin/Visual Studio Code Projects/CMPM 123/cmpm123
+# Build directory: C:/Users/Justin/Visual Studio Code Projects/CMPM 123/cmpm123/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
